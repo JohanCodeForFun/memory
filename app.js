a2b40f38f72fe99ef251704135db9a7a6191827f
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-07-06 20:08:17 +0200

    Restart

---
+++ app.js
@@ -0,0 +1,13 @@
+// Grab a couple of things
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector('span');
+const playerLives = 6;
+
+// link text
+playerLivesCount.textContent = playerLives;
+
+// generate the data
+
+const getData = () => [{}]
+
+const data = getData();
