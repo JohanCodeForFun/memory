579d6c8a98b600572cd273849c9c905cc6fa032d
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-07-07 22:01:49 +0200

    basic game done!

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // Grab a couple of things
 const section = document.querySelector('section');
 const playerLivesCount = document.querySelector('span');
-let playerLives = 6;
+let playerLives = 10;
 
 // link text
 playerLivesCount.textContent = playerLives;
 
 // generate the data
-
 const getData = () => [
     { imgSrc: "./images/IMG_2392.jpeg", name: "cafe" },
     { imgSrc: "./images/IMG_4070.jpeg", name: "swings" },
@@ -46,6 +45,7 @@ const cardGenerator = () => {
         back.classList = 'back';
         //Attach the info to the cards
         face.src = item.imgSrc;
+        card.setAttribute('name', item.name);
         //Attach the cards to the section
         section.appendChild(card);
         card.appendChild(face);
@@ -57,11 +57,65 @@ const cardGenerator = () => {
         });
     });
 };
+
 //Check Cards
 const checkCards = (e) => {
     const clickedCard = e.target;
-    console.log(clickedCard);
-    // at 30:00 minutes in.
-}
+    clickedCard.classList.add('flipped');
+    const flippedCards = document.querySelectorAll('.flipped');
+    const toggleCard = document.querySelectorAll('.toggleCard');
+    //Logic
+    if (flippedCards.length === 2) {
+        if (flippedCards[0].getAttribute("name") ===
+        flippedCards[1].getAttribute("name")) {
+            console.log('match');
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                card.style.pointerEvents = "none";
+            })
+        } else {
+            console.log("wrong");
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                setTimeout(() => card.classList.remove("toggleCard"), 1000);
+            });
+            playerLives--;
+            playerLivesCount.textContent = playerLives;
+            if (playerLives === 0) {
+                restart('Försök igen');
+            }
+        }
+    }
+    //Run a check to see if we won the game
+    if (toggleCard.length === 16) {
+        restart('Bra jobbat!');
+    }
+};
+
+//Restart
+const restart = (text) => {
+    let cardData = randomize();
+    let faces = document.querySelectorAll('.face');
+    let cards = document.querySelectorAll('.card');
+    section.style.pointerEvents = 'none';
+    cardData.forEach((item, index) => {
+        cards[index].classList.remove('toggleCard');
+        //Randomize
+        setTimeout(() => {
+            cards[index].style.pointerEvents = 'all';
+            faces[index].src = item.imgSrc;
+            cards[index].setAttribute('name', item.name);
+            section.style.pointerEvents = 'all';
+        }, 1000);
+
+    });
+    playerLives = 10;
+    playerLivesCount.textContent = playerLives;
+    setTimeout(() => window.alert(text), 100);
+};
 
 cardGenerator();
+
+// Remember to check how it works on mobile
+// If needed, modify grid to 2 rows on mobile
+// create a zoom button for 2 column big images
